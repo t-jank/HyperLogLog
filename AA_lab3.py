7befe3390f935bf6e22ee2b16d1958204db7ab95
TomaszJankowiak <62778369+TomaszJankowiak@users.noreply.github.com>
2022-04-20 13:52:55 +0200

    pelno bledow nic nie dziala, ale calka chyba ok xd

--- AA_lab3.py
+++ AA_lab3.py
@@ -1,12 +1,20 @@
 import math
 import hashlib
 import matplotlib.pyplot as plt
+import scipy.integrate
+import numpy
+
 
 ######### zadanie 8 ###########
 
-def HyperLogLog(multizbior,m,h):
+def integrand(u,m):
+    return math.log((2+u)/(1+u),2)**m
+
+def HyperLogLog(multizbior,m,h): # m - liczba podstrumieni, potega dwojki
     Z=0
     M=[]
+    b = math.log(m,2)
+    alfam = ( m * scipy.integrate.quad(integrand, 0, numpy.inf, args=(m))[0] )**(-1)
     for i in range(0,m):
         M.append(0)
     for e in range(0,len(multizbior)):
@@ -29,6 +37,18 @@ def HyperLogLog(multizbior,m,h):
 
     return nzd
 
+def g(x): # zwraca pozycje pierwszej jedynki od lewej strony
+    g=0
+    while x[g]!=49:
+        g+=1
+    return g+1
 
 def md5(x):
     return int(hashlib.md5(str(x).encode()).hexdigest(),16)/2**128
+
+'''
+def hash_sha256_hll(x):
+    x= str(int(x)).encode('utf-8')
+    return hashlib.sha256(x).hexdigest()
+'''
+#HyperLogLog([1,2],32,md5)
